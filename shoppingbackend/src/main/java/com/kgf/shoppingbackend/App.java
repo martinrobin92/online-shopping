207646773aa3bb6bef4798e187a7f172a9e9b518
martin robin <martinrobin1414@gmail.com>
2018-11-22 13:08:27 +0530

    hi my project setup is completed

---
+++ shoppingbackend/src/main/java/com/kgf/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.kgf.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
